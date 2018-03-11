b804c4202de137a12d68ecfeee754ead5ac9057c
Antoine Gillot <antoine.gillot60@gmail.com>
2018-03-11 15:57:56 +0100

    fix demande inscription par user

--- src/UserBundle/Form/Type/UserRegistrationType.php
+++ src/UserBundle/Form/Type/UserRegistrationType.php
@@ -2,6 +2,8 @@
 
 namespace UserBundle\Form\Type;
 
+use DepotBundle\DepotBundle;
+use FOS\UserBundle\Model\Group;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -9,24 +11,28 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use DepotBundle\Entity\Groupe;
 
 class UserRegistrationType extends AbstractType
 {
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $entityManager = $options['entity_manager'];
+        $groupes = $entityManager->getRepository('DepotBundle:Groupe')->findAll();
+        $choice_groupes = [];
+        foreach ($groupes as $groupe) {
+            $choice_groupes[$groupe->getName()] = $groupe->getId();
+        }
         $builder->add('first_name', TextType::class, array("label" => "Prénom"))
-        ->add('last_name', TextType::class, array("label" => "Nom"))
-        ->add('email', EmailType::class, array("label" => "E-mail"))
-        ->add('groupes', ChoiceType::class, array(
-            'label' => "UE",
-            'multiple' => true,
-            'choices' => array(
-                'M2.45 - ASI' => '1',
-                'M2.34 - Green IT' => '2',
-                'M2.6 - Projet thématique' => '3'
-            )
-        ))
-        ->add('submit', SubmitType::class, array("label" => "Valider"));
+            ->add('last_name', TextType::class, array("label" => "Nom"))
+            ->add('email', EmailType::class, array("label" => "E-mail"))
+            ->add('groupes', ChoiceType::class, array(
+                'label' => "Groupe",
+                'multiple' => true,
+                'choices' => $choice_groupes
+            ))
+            ->add('submit', SubmitType::class, array("label" => "Valider"));
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -34,5 +40,7 @@ class UserRegistrationType extends AbstractType
         $resolver->setDefaults(array(
             'data_class' => 'UserBundle\Entity\User'
         ));
+        $resolver->setRequired('entity_manager');
+
     }
 }
