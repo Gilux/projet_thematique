5072c3d8a6a78d164a358a2a3c34a142d4cc30bc
Antoine Gillot <antoine.gillot60@gmail.com>
2018-04-30 12:01:12 +0200

    with render controller

--- src/DepotBundle/Controller/DefaultController.php
+++ src/DepotBundle/Controller/DefaultController.php
@@ -10,9 +10,18 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $user = $this->getDoctrine()->getRepository("UserBundle:User")->find($this->getUser());
+        return $this->render('DepotBundle:Default:index.html.twig', array('user' => $user));
+    }
+
+    public function getNotifications()
+    {
+        $user = $this->getDoctrine()->getRepository("UserBundle:User")->find($this->getUser());
         $manager = $this->get('mgilet.notification');
         $entity = $manager->getNotifiableEntity($user);
         $interface = $manager->getNotifiableInterface($entity);
-        return $this->render('DepotBundle:Default:index.html.twig', array("notifiableInterface" => $interface, "notifiableNotifications" => $entity->getnotifiableNotifications(), "user" => $user));
+        return $this->render(
+            'notification/notifications.html.twig',
+            array("notifiableInterface" => $interface, "notifiableNotifications" => $entity->getnotifiableNotifications(), "user" => $user)
+        );
     }
 }
