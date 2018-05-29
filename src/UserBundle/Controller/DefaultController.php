b3b07987fa69f967288915b6b5681daa8c07b512
vincentpochon <vincentpochon@orange.fr>
2018-05-29 18:18:53 +0200

    Redirection si l'on est connecté

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -8,6 +8,17 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('UserBundle:Default:index.html.twig');
+        if ($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute("users_admin");
+        }
+        else if($this->get('security.authorization_checker')->isGranted('ROLE_ENSEIGNANT')){
+            return $this->redirectToRoute("depot_homepage");
+        }
+        else if($this->get('security.authorization_checker')->isGranted('ROLE_ETUDIANT')){
+            return $this->redirectToRoute("depot_homepage");
+        }
+        else {
+            return $this->render('UserBundle:Default:index.html.twig');
+        }
     }
 }
