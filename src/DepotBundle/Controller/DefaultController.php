0650f54f86cd6e49c9c6293a240d8a3c38f07618
Antoine Gillot <antoine.gillot60@gmail.com>
2018-06-01 07:57:41 +0200

    fix display groupes et rendus

--- src/DepotBundle/Controller/DefaultController.php
+++ src/DepotBundle/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace DepotBundle\Controller;
 
+use DepotBundle\Entity\Devoir;
 use DepotBundle\Entity\Groupe;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use DepotBundle\Controller\DevoirController;
 
 class DefaultController extends Controller
 {
@@ -28,14 +30,16 @@ class DefaultController extends Controller
             $groupes_devoir = $g->getGroupeDevoir();
             foreach ($groupes_devoir as $gd) {
                 $gpRepo = $this->getDoctrine()->getRepository("DepotBundle:Groupe_projet");
-                $groupes_rendus = $gpRepo->findByDevoir($gd->getDevoir());
-                $gp = $this->getDoctrine()->getRepository("DepotBundle:Groupe_projet")->findByDevoirAndUser($gd->getDevoir(), $user);
 
+                $devoir = $gd->getDevoir();
+                $groupes_rendus = $gpRepo->findByDevoir($devoir);
+
+                $users_status = $this->getUsersStatusInDevoir($devoir);
+                $devoir->users_status = $users_status;
+
+                $gp = $this->getDoctrine()->getRepository("DepotBundle:Groupe_projet")->findByDevoirAndUser($devoir, $user);
                 $data[] = [
-                    "id" => $gd->getDevoir()->getId(),
-                    "ue" => $gd->getDevoir()->getUe()->getNom(),
-                    "user" => $gd->getDevoir()->getUser(),
-                    "titre" => $gd->getDevoir()->getTitre(),
+                    "devoir" => $devoir,
                     "groupe" => $groupes_rendus,
                     "date_rendu" => $gd->getDateARendre(),
                     "date_rendu_file" => $gp ? $gp->getDate() : false,
@@ -47,6 +51,48 @@ class DefaultController extends Controller
         return $this->render('DepotBundle:Default:index.html.twig', array('data' => $data));
     }
 
+    private function getUsersStatusInDevoir(Devoir $devoir)
+    {
+        $groupes_projet = $this->getDoctrine()->getRepository("DepotBundle:Groupe_projet")->findByDevoir($devoir);
+
+        $groupes_devoir = $this->getDoctrine()->getRepository("DepotBundle:Groupe_devoir")->findByDevoir($devoir);
+
+        $ugp_repo = $this->getDoctrine()->getRepository("DepotBundle:Groupe_devoir");
+        $users_in_project = [];
+        $users_not_in_groupe_projet = [];
+        $users_render = 0;
+
+        foreach ($groupes_devoir as $gp_devoir) {
+
+            $users = $gp_devoir->getGroupe()->getUsers();
+
+            foreach ($users as $user) {
+                $flag = false;
+
+                foreach ($groupes_projet as $gp) {
+
+                    $usr = $ugp_repo->findBy(["groupe_projet" => $gp, "user" => $user]);
+
+                    if ($usr) {
+                        $users_in_project[] = $usr;
+                        $flag = true;
+
+                        if ($gp->getFichier() != '') {
+                            $users_render++;
+                        }
+                    }
+                }
+                if (!$flag) {
+                    $users_not_in_groupe_projet[] = $user;
+                }
+            }
+        }
+        return [
+            'users_out' => $users_not_in_groupe_projet,
+            'users_in' => $users_in_project,
+            'users_render' => $users_render
+        ];
+    }
 
     public function getEnseignantDevoirsAction()
     {
@@ -56,17 +102,21 @@ class DefaultController extends Controller
         $data = $dates_a_rendre = [];
         $user = $this->getUser();
 
+
         foreach ($user->getUes() as $ue) {
             foreach ($ue->getDevoirs() as $devoir) {
+
+                $users_status = $this->getUsersStatusInDevoir($devoir);
+                $devoir->users_status = $users_status;
                 $devoir->nombre_rendus = 0;
-                $devoir->nombre_groupes_devoir = 0;
+                $devoir->nombre_groupes_projet = 0;
                 //pour chaque devoir compter le nombre de rendus
                 $gps = $repo_groupe_projet->findByDevoir($devoir);
                 foreach ($gps as $gp) {
                     if ($gp->getFichier() != null) {
                         $devoir->nombre_rendus++;
                     }
-                    $devoir->nombre_groupes_devoir++;
+                    $devoir->nombre_groupes_projet++;
 
                     $groupe = $gp->getGroupe();
 
