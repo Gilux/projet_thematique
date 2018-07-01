3eea136c7aea69867fdb4f70c7e6307f55bf001a
Gilles <gilles.granger@outlook.fr>
2018-07-01 12:46:02 +0200

    fix params

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -12,9 +12,16 @@ $container->setParameter('database_password', $db['pass']);
 $container->setParameter('secret', getenv('SECRET'));
 $container->setParameter('locale', 'en');
 
+//$container->setParameter('mailer_transport', 'smtp');
+//$container->setParameter('mailer_host', 'smtp-mail.outlook.com');
+//$container->setParameter('mailer_port', '587');
+//$container->setParameter('mailer_user', "miage_depot_devoirs@outlook.fr");
+//$container->setParameter('mailer_password', getenv('EMAIL_PASSWORD'));
+//$container->setParameter('mailer_encryption', 'tls');
+
 $container->setParameter('mailer_transport', 'smtp');
-$container->setParameter('mailer_host', 'smtp-mail.outlook.com');
-$container->setParameter('mailer_port', '587');
-$container->setParameter('mailer_user', "miage_depot_devoirs@outlook.fr");
+$container->setParameter('mailer_host', 'smtp.gmail.com');
+$container->setParameter('mailer_port', '25');
+$container->setParameter('mailer_user', "depot.devoir.miage@gmail.com");
 $container->setParameter('mailer_password', getenv('EMAIL_PASSWORD'));
 $container->setParameter('mailer_encryption', 'tls');
